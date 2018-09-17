3b015c9b9435b1263bb199ad294801ec240d45fa
Tomasz Juchniewicz <tjuchniewicz@gmail.com>
2018-09-17 11:02:58 +0200

    Fix checkstyle issues

--- src/main/java/com/hazelcast/eureka/one/EurekaHazelcastMetadata.java
+++ src/main/java/com/hazelcast/eureka/one/EurekaHazelcastMetadata.java
@@ -24,7 +24,7 @@ final class EurekaHazelcastMetadata {
     static final String HAZELCAST_HOST = "hazelcast.host";
     static final String HAZELCAST_PORT = "hazelcast.port";
     static final String HAZELCAST_GROUP_NAME = "hazelcast.groupName";
-    
+
     private EurekaHazelcastMetadata() {
         // class with constants only
     }
